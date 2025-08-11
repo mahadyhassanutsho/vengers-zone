8019a5bd9466e06b7760d241609d67e38bd53313
Utsho MH <mahadyhassanutsho@gmail.com>
2025-08-11 21:33:46 +0600

    added loader and loading animation

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const slides = document.querySelectorAll(".hero-slider img");
+const loaderContainer = document.querySelector(".loader-container");
+const pageContent = document.querySelector(".page-content");
 const timeInBetweenSlide = 3000;
 let slideIndex = 0;
+
+window.addEventListener("load", () => {
+  loaderContainer.classList.add("hidden");
+  pageContent.classList.add("visible");
+});
 
 const showSlide = (index) =>
   slides.forEach((slide, i) => {
