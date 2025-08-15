9ccc5f7cc985688dbfb719a5a517665a18b8590c
Utsho MH <mahadyhassanutsho@gmail.com>
2025-08-15 21:17:02 +0600

    hamburger menu and full-responsiveness

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const slides = document.querySelectorAll(".hero-slider img");
 const loaderContainer = document.querySelector(".loader-container");
 const pageContent = document.querySelector(".page-content");
+const hamburger = document.querySelector(".hamburger");
+const navList = document.querySelector(".main-nav .nav-list");
+
 const timeInBetweenSlide = 3000;
 let slideIndex = 0;
 
 window.addEventListener("load", () => {
   loaderContainer.classList.add("hidden");
   pageContent.classList.add("visible");
+});
+
+hamburger.addEventListener("click", () => {
+  navList.classList.toggle("visible");
+  hamburger.classList.toggle("active");
+});
+
+navList.addEventListener("click", () => {
+  navList.classList.toggle("visible");
+  hamburger.classList.toggle("active");
 });
 
 const showSlide = (index) =>
