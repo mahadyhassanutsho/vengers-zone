94418ecf662fe23fb44700086f55570918feb05e
Utsho MH <mahadyhassanutsho@gmail.com>
2025-08-16 12:03:38 +0600

    fixed (#4) hamburger menu closing issue

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const loaderContainer = document.querySelector(".loader-container");
 const pageContent = document.querySelector(".page-content");
 const hamburger = document.querySelector(".hamburger");
 const navList = document.querySelector(".main-nav .nav-list");
+const navLinks = document.querySelectorAll(".main-nav .nav-list a.nav-link");
 
 const timeInBetweenSlide = 3000;
 let slideIndex = 0;
@@ -17,10 +18,12 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
 });
 
-navList.addEventListener("click", () => {
-  navList.classList.toggle("visible");
-  hamburger.classList.toggle("active");
-});
+navLinks.forEach((link) =>
+  link.addEventListener("click", () => {
+    navList.classList.toggle("visible");
+    hamburger.classList.toggle("active");
+  })
+);
 
 const showSlide = (index) =>
   slides.forEach((slide, i) => {
